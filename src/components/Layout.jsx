1820c24ca959f25fe089af23a28f3215837cfe45
MarieIT <142742570+MarieIT@users.noreply.github.com>
2025-03-15 22:16:49 +0100

    added BrowserRoute and html main to layout

---
+++ src/components/Layout.jsx
@@ -0,0 +1,23 @@
+
+<>
+<main>
+<article id="article-card">
+    <h2>HTML</h2>
+    <p>
+        HTML står for HyperText Markup Language, 
+        og er et strukturspråk som brukes for å lage strukturer til nettside- og applikasjonsgrensesnitt.
+    </p>  
+    <ul>
+        <li>
+            <a href="#">W3Schools</a>
+        </li>
+        <li>
+            <a href="#">HTML Living standard</a>
+        </li>
+        <li>
+            <a href="#">HTML.com Tutorials</a>
+        </li>
+    </ul>
+</article>
+</main>
+</>
